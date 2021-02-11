9ed47beaddcc94b81511c54c0156830c24729b4b
Daetwen <62154512+Daetwen@users.noreply.github.com>
2021-02-11 22:03:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = 0;
+    n = Math.abs(n);
+    let length = ('' + n).length;
+    for(i = 0; i < length; i++){
+        result = result * 10;
+        result += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return result;
 }
